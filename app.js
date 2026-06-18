86aa584217b495f620c582a5f66f372493b0e558
maji <maji@tyo.taile44373.ts.net>
2026-06-18 11:20:54 +0900

    feat: 件数バッジ追加・カード下端グラデーションフェード

--- app.js
+++ app.js
@@ -213,12 +213,12 @@ function buildPopup(v) {
     <div class="popup-box">
       <span class="popup-badge ${headLabel === 'exception' || headLabel === 'cancel' ? 'exception-badge' : ''}"
             style="background:${badgeColors[headLabel]}">${badgeTexts[headLabel]}</span>
-      <div class="popup-name">🏢 ${v.facility_name || headName}</div>
+      <div class="popup-name">🏢 ${v.facility_name || headName}${meetings && meetings.length > 1 ? '<span class="meeting-count-badge">' + meetings.length + '件</span>' : ''}</div>
       ${addr ? `<div class="popup-address">📍 ${addr}</div>` : ''}
       ${verifyNotice}
-      <div class="meetings-list">
+      <div class="meetings-wrapper"><div class="meetings-list">
         ${meetingsHTML}
-      </div>
+      </div></div>
       <div class="popup-links">
         ${calLink}
         ${mapsLink}
